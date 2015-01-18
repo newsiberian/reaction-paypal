2c9aece7db375c57851a34e0ca6bb69b02791a09
aaronjudd <aaronjudd@me.com>
2015-01-18 12:02:41 -0800

    Updated versions

--- package.js
+++ package.js
@@ -9,7 +9,7 @@ Npm.depends({'paypal-rest-sdk': '1.2.2'});
 
 Package.onUse(function (api, where) {
   api.versionsFrom('METEOR@1.0');
-  api.use("meteor-platform");
+  api.use("meteor-platform@1.2.1");
   api.use("coffeescript");
   api.use("less");
   api.use("reactioncommerce:core@0.2.2");
