300c95852a24a3cf4ca2a080b305b63158ce61d7
aaronjudd <aaronjudd@me.com>
2015-02-02 23:23:05 -0800

    update version 0.2.0

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   summary: "Reaction Paypal - PayPal payments for Reaction Commerce",
   name: "reactioncommerce:reaction-paypal",
-  version: "0.1.4",
+  version: "0.2.0",
   git: "https://github.com/reactioncommerce/reaction-paypal.git"
 });
 
