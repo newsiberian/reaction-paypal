c0c071da7b29b1d85585593b6064276c2bc1aa4a
aaronjudd <aaronjudd@me.com>
2015-03-20 21:58:05 -0700

    updated package version, published express
    published package 0.5.0 is this ‘express-checkout’ branch, version
    0.5.0
    development branch is not yet compatible

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   summary: "Reaction Paypal - PayPal payments for Reaction Commerce",
   name: "reactioncommerce:reaction-paypal",
-  version: "0.4.1",
+  version: "0.5.0",
   git: "https://github.com/reactioncommerce/reaction-paypal.git",
   icon: 'fa fa-paypal'
 });
