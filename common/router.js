49771be9b8ade420de70894a9a4e6261f5a79907
Mike Murray <hellomikemurray@gmail.com>
2015-09-07 21:56:38 -0700

    Standardization of settings and dashboard views

--- common/router.js
+++ common/router.js
@@ -1,5 +1,5 @@
 Router.map(function() {
-  this.route('paypal', {
+  this.route('dashboard/paypal', {
     controller: ShopSettingsController,
     path: '/dashboard/paypal',
     template: 'paypalDashboard',
--- server/register.js
+++ server/register.js
@@ -1,32 +1,34 @@
 ReactionCore.registerPackage({
-  label: 'PayPal',
-  name: 'reaction-paypal',
-  icon: 'fa fa-paypal',
+  label: "PayPal",
+  name: "reaction-paypal",
+  icon: "fa fa-paypal",
   autoEnable: false,
   registry: [
     {
-      provides: 'dashboard',
-      label: 'PayPal',
-      route: 'paypal',
-      description: 'PayPal Payment for Reaction Commerce',
-      icon: 'fa fa-paypal',
-      cycle: '3',
-      container: 'reaction-paypal'
+      provides: "dashboard",
+      label: "PayPal",
+      route: "dashboard/paypal",
+      description: "PayPal Payment for Reaction Commerce",
+      route: "dashboard/paypal",
+      icon: "fa fa-paypal",
+      cycle: "3",
+      container: "reaction-paypal"
     }, {
-      label: 'PayPal Settings',
-      provides: 'settings',
-      container: 'reaction-paypal',
-      template: 'paypalSettings'
+      label: "PayPal Settings",
+      route: "dashboard/paypal",
+      provides: "settings",
+      container: "reaction-paypal",
+      template: "paypalSettings"
     }, {
-      template: 'paypalPaymentForm',
-      provides: 'paymentMethod'
+      template: "paypalPaymentForm",
+      provides: "paymentMethod"
     }
   ],
   permissions: [
     {
-      label: 'PayPal',
-      permission: 'dashboard/payments',
-      group: 'Shop Settings'
+      label: "PayPal",
+      permission: "dashboard/payments",
+      group: "Shop Settings"
     }
   ]
 });
