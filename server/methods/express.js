21c52723cfafde5e8f677ef6ba5d16176df10421
Brent Hoover <brent@thebuddhalodge.com>
2015-12-03 12:46:51 +0800

    Implemented RefundList

--- server/methods/express.js
+++ server/methods/express.js
@@ -1,4 +1,4 @@
-var parseResponse;
+var parseResponse, parseRefundReponse;
 var nvpVersion = '52.0';
 
 Meteor.methods({
@@ -170,7 +170,7 @@ Meteor.methods({
   "paypalexpress/refund/create": function(paymentMethod, amount) {
     check(paymentMethod, ReactionCore.Schemas.PaymentMethod);
     check(amount, Number);
-    var result, response, options, error, transactionId, currencycode;
+    var result, response, options, error, transactionId, currencycode, amount_formatted;
     this.unblock();
     options = Meteor.Paypal.expressCheckoutAccountOptions();
     var previousTransaction = paymentMethod.transactions[1];
@@ -197,7 +197,62 @@ Meteor.methods({
     }
 
     if (!response || response.statusCode !== 200) {
-      throw new Meteor.Error('Bad Response from Paypal during Capture');
+      throw new Meteor.Error('Bad Response from Paypal during Refund Creation');
+    }
+
+    response = parseResponse(response);
+    if (response.ACK !== 'Success') {
+      throw new Meteor.Error('ACK ' + response.ACK + ': ' + response.L_LONGMESSAGE0);
+    }
+
+    amount_formatted = {
+      total: amount,
+      currency: 'USD'
+    };
+
+    result = {
+      saved: true,
+      type: 'refund',
+      created: new Date(),
+      transactionId: transactionId,
+      refundTransactionId: response.REFUNDTRANSACTIONID,
+      grossRefundAmount: response.GROSSREFUNDAMT,
+      netRefundAmount: response.NETREFUNDAMT,
+      correlationId: response.CORRELATIONID,
+      currency: response.CURRENCYCODE,
+      amount: amount_formatted,
+      rawTransaction: response
+    };
+
+    return result;
+
+  },
+  "paypalexpress/refund/list": function(paymentMethod) {
+    check(paymentMethod, ReactionCore.Schemas.PaymentMethod);
+    var transactionId, response, result, error, options;
+    this.unblock();
+    options = Meteor.Paypal.expressCheckoutAccountOptions();
+    transactionId = paymentMethod.transactionId;
+    try {
+      response = HTTP.post(options.url, {
+        params: {
+          USER: options.username,
+          PWD: options.password,
+          SIGNATURE: options.signature,
+          VERSION: nvpVersion,
+          METHOD: 'TransactionSearch',
+          STARTDATE: '2013-08-24T05:38:48Z',
+          TRANSACTIONID: transactionId,
+          TRANSACTIONCLASS: 'Refund'
+        }
+      });
+    }  catch (_error) {
+      error = _error;
+      throw new Meteor.Error(error.message);
+    }
+
+    if (!response || response.statusCode !== 200) {
+      throw new Meteor.Error('Bad Response from Paypal during refund list');
     }
 
     response = parseResponse(response);
@@ -205,28 +260,8 @@ Meteor.methods({
     if (response.ACK !== 'Success') {
       throw new Meteor.Error('ACK ' + response.ACK + ': ' + response.L_LONGMESSAGE0);
     }
-
-    result = {
-      saved: true,
-      transactionId: transactionId,
-      refundTransactionId: response.REFUNDTRANSACTIONID,
-      grossRefundAmount: response.GROSSREFUNDAMT,
-      netRefundAmount: response.NETREFUNDAMT,
-      correlationId: response.CORRELATIONID,
-      currencycode: currencycode,
-      amount: amount,
-      metadata: {},
-      rawTransaction: response
-    };
-
+    result = parseRefundReponse(response);
     return result;
-
-
-
-  },
-  "paypalexpress/refund/list": function() {
-    console.log('Executed stub function for refund/list');
-    return [];
   }
 
 });
@@ -241,4 +276,33 @@ parseResponse = function(response) {
     return result[subpieces[0]] = decodeURIComponent(subpieces[1]);
   });
   return result;
+};
+
+parseRefundReponse = function(response) {
+  var timeStampKey, timestamp, typeKey, transactionType, amountKey, amount, currencyCodeKey, currencyCode, responseObject;
+  var paypalArray = [];
+
+  for (var i=0; i<101; i++) {
+    timeStampKey = 'L_TIMESTAMP' + i;
+    timestamp = response[timeStampKey];
+    typeKey = 'L_TYPE' + i;
+    transactionType = response[typeKey];
+    amountKey = 'L_AMT' + i;
+    amount = response[amountKey];
+    currencyCodeKey = 'L_CURRENCYCODE' + i;
+    currencyCode = response[currencyCodeKey];
+
+    if(timestamp !== undefined && transactionType === 'Refund') {
+      responseObject = {
+        created: moment(timestamp).valueOf(),
+        type: 'refund',
+        amount: Math.abs(Number(amount, 10)),
+        currency: currencyCode
+      };
+      paypalArray.push(responseObject);
+
+    }
+  }
+
+  return paypalArray;
 };
