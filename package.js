f8e338f556f553febde51acd19af6c0ea18ec1e5
Brent Hoover <brent@thebuddhalodge.com>
2015-12-07 15:42:36 +0800

    Bump Versions

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   summary: "Reaction Paypal - PayPal payments for Reaction Commerce",
   name: "reactioncommerce:reaction-paypal",
-  version: "1.2.4",
+  version: "1.2.5",
   git: "https://github.com/reactioncommerce/reaction-paypal.git"
 });
 
